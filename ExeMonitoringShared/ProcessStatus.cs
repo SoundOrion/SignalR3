c62e771448877c936faa1874c7b4b32bd0d408e1
hatsuyama <hatsuyama>
2025-02-15 06:25:34 +0900

    edit

---
+++ ExeMonitoringShared/ProcessStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace ExeMonitoringShared;
+
+public class ProcessStatus
+{
+    public string ClientId { get; set; } = string.Empty;
+    public bool IsRunning { get; set; }
+    public DateTime LastUpdated { get; set; } // ✅ UI 側で使用する
+}
